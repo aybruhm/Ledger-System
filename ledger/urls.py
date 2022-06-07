b79978e271fd5179cfaeec055d50ef010b042d23
Israel Abraham <israelvictory87@gmail.com>
2022-06-08 00:09:05 +0100

    Created GetUserBalance, GetAccountBalance, CreateUser and CreateUserAccount url paths

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 
 # App Imports
-from ledger.views import Deposit
+from ledger.views import Deposit, Withdraw, CreateUserAccount, CreateUser,\
+    GetUserBalance, GetAccountBalance
 
 
 app_name = "ledger"
@@ -10,4 +11,10 @@ app_name = "ledger"
 
 urlpatterns = [
     path("deposit/", Deposit.as_view(), name="deposit"),
+    path("withdraw/", Withdraw.as_view(), name="withdraw"),
+    
+    path("create-user-account/", CreateUserAccount.as_view(), name="create-account"),
+    path("create-user/", CreateUser.as_view(), name="create-user"),
+    path("user-balance/", GetUserBalance.as_view(), name="user-balance"),
+    path("account-balance/", GetAccountBalance.as_view(), name="account-balance"),
 ]
