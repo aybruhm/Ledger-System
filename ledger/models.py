cd4f58c4e80bdadc00e5ada7a32597e6a6ad6821
Israel Abraham <israelvictory87@gmail.com>
2022-06-07 23:29:59 +0100

    Created Transaction model

--- ledger/models.py
+++ ledger/models.py
@@ -17,24 +17,45 @@ class User(TimeStampModel):
     class Meta:
         verbose_name_plural = "Users"
         db_table = "users"
-      
         
-class Account(TimeStampModel):
+        
+class Transaction(TimeStampModel):
     
     TRANSACTION_TYPES = (
         ("deposit", "desposit"),
         ("withdraw", "withdraw"),
         ("transfer", "transfer")
     )
+    name = models.CharField(max_length=255, unique=True, help_text="account name to deposit")
+    slug = models.SlugField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField(default=0.0)
+    type = models.CharField(choices=TRANSACTION_TYPES, max_length=10)
+    
+    def __str__(self) -> str:
+        return "Transaction for {}".format(self.name)
+    
+    def save(self, *args, **kwargs):
+        
+        if not self.slug:
+            self.slug = slugify(self.name)
+        
+        super(Transaction, self).save(*args, **kwargs)
+        
+    class Meta:
+        verbose_name_plural = "User Transactions"
+        db_table = "transactions"
+        
+    
+class Account(TimeStampModel):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     available_amount = models.FloatField(default=0.0)
-    type = models.CharField(choices=TRANSACTION_TYPES, max_length=10)
     
     def __str__(self) -> str:
         return "Account for {}".format(self.name)
-    
+          
     def save(self, *args, **kwargs):
         
         if not self.slug:
@@ -43,5 +64,5 @@ class Account(TimeStampModel):
         super(Account, self).save(*args, **kwargs)
         
     class Meta:
-        verbose_name_plural = "Accounts"
+        verbose_name_plural = "User Accounts"
         db_table = "accounts"
