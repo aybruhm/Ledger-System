dfce49c2fdcc1ad862adfeadf4b699d1c5cd3e3c
Israel Abraham <israelvictory87@gmail.com>
2022-06-08 00:53:03 +0100

    Update CreateTransactionSerializer

--- ledger/serializers.py
+++ ledger/serializers.py
@@ -2,41 +2,21 @@
 from rest_framework import serializers
 
 # App Imports
-from ledger.models import Account, User
+from ledger.models import Transaction, User, Account
 
 
-class TransactAccount(serializers.ModelSerializer):
+class CreateTransactionSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = Account
-        fields = ("name", "user", "amount", "type")
+        model = Transaction
+        fields = ("account", "user", "amount", "type")
         
-    def create(self, validated_data):
-        """
-        It gets the validated data, gets the account name and user, 
-        and then increases the available amount with the deposited amount
-        
-        :param validated_data: The validated data from the serializer
-        :return: The account object is being returned.
-        """
-        
-        account_name = validated_data.get("name")
-        account_user = validated_data.get("user")
-        amount = validated_data.get("amount")
-        
-        # Get user and account name, 
-        # and then increase available amount with deposited amount
-        account = Account.objects.get(name=account_name, user=account_user)
-        account.available_amount += amount
-        
-        return account
-
 
 class AccountSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Account
-        fields = ("id", "name", "user", "available_amount", "type",)
+        fields = ("id", "name", "user")
         
 
 class UserSerializer(serializers.ModelSerializer):
