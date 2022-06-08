17a2a805078c341a228f8fe52a2fffa2c757a030
Israel Abraham <israelvictory87@gmail.com>
2022-06-08 01:34:10 +0100

    Created AccountToUserTransfer url path

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 
 # App Imports
-from ledger.views import Deposit, Withdraw, CreateUserAccount, CreateUser,\
+from ledger.views import Deposit, Withdraw, CreateUserAccount, \
+    AccountToUserTransfer, CreateUser,\
     GetUserBalance, GetAccountBalance
 
 
@@ -12,7 +13,7 @@ app_name = "ledger"
 urlpatterns = [
     path("deposit/", Deposit.as_view(), name="deposit"),
     path("withdraw/", Withdraw.as_view(), name="withdraw"),
-    
+    path("account-to-user-transfer/", AccountToUserTransfer.as_view(), name="account-to-user-transfer"),
     path("create-user-account/", CreateUserAccount.as_view(), name="create-account"),
     path("create-user/", CreateUser.as_view(), name="create-user"),
     path("user-balance/<int:user>/", GetUserBalance.as_view(), name="user-balance"),
