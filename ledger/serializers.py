fdb567fc1317e6ba7c0c3272ce29c2e6446777b8
Israel Abraham <israelvictory87@gmail.com>
2022-06-08 16:49:31 +0100

    Created create method for UserSerializer

--- ledger/serializers.py
+++ ledger/serializers.py
@@ -12,6 +12,19 @@ class CreateTransactionSerializer(serializers.ModelSerializer):
         fields = ("account", "to_account", "user", "to_user", "amount", "type")
         
 
+class TransferTransactionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Transaction
+        fields = ("to_account", "to_user", "amount", "type")
+        
+    def validate_from_user_accounts(self, value):
+        pass
+    
+    def validate_to_user_account(self, value):
+        pass
+
+
 class AccountSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -23,4 +36,10 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ("id", "firstname", "lastname", "username")
+        fields = ("id", "firstname", "lastname", \
+            "email", "username", "password")
+        
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.save()
+        return user
